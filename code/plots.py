bbcf8f938aaea2e8956d2c1a5d90973f638e4c0a
PHouzel <71659863+PHouzel@users.noreply.github.com>
2022-03-12 18:24:20 +0100

    Cleaning up

--- code/plots.py
+++ code/plots.py
@@ -47,10 +47,3 @@ def plt_fmri_stim(X, Y, pathfile):
         fig.savefig(pathfile)
     
     return
-
-import re
-
-def natural_sort_key(s, _nsre=re.compile('([0-9]+)')):
-    return [int(text) if text.isdigit() else text.lower() for text in _nsre.split(s)]
- 
-    
